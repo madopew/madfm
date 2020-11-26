dbc2195f84a29b7264d5ee385d29c4f02f7af8e4
Bakyt Madi <bakyt.madi.work@gmail.com>
2020-11-26 19:22:00 +0300

    Make output faster

--- src/gui/modules/FilePreviewer.cpp
+++ src/gui/modules/FilePreviewer.cpp
@@ -28,11 +28,12 @@ void FilePreviewer::showTextPreview() {
     cgh->utils.clearScreen();
     int c;
     int skip_start = 0;
+    std::string output;
     for(int i = 0; i < rows - 1; i++) {
         for(int j = 0; j < columns; j++) {
             if(j == 0 && !skip_start) {
-                cgh->utils.outputChar('~', cgh->saved_attributes);
-                cgh->utils.outputChar(' ', cgh->saved_attributes);
+                output.push_back('~');
+                output.push_back(' ');
                 j++;
                 continue;
             }
@@ -40,23 +41,24 @@ void FilePreviewer::showTextPreview() {
             if(c == '\n' || c == EOF) {
                 skip_start = 0;
                 if(i != rows - 2)
-                    cgh->utils.outputChar('\n', cgh->saved_attributes);
+                    output.push_back('\n');
                 break;
             } else if(c == '\t') {
                 for(int k = 0; k < 4; k++) {
-                    cgh->utils.outputChar(' ', cgh->saved_attributes);
+                    output.push_back(' ');
                 }
                 j+=3;
             } else {
-                cgh->utils.outputChar(c, cgh->saved_attributes);
+                output.push_back(c);
                 if(j == columns - 1)
                     skip_start = 1;
             }
         }
     }
     if(c != EOF)
-        cgh->utils.outputLine(PRE_NOTEND, cgh->saved_attributes);
+        output.append(PRE_NOTEND);
 
+    cgh->utils.outputLineNoNew(output, cgh->saved_attributes);
     waitForClose();
     fclose(f);
     cgh->cleanRedrawConsoleGui();
@@ -67,50 +69,47 @@ void FilePreviewer::showRawPreview() {
     if(!f) {
         return;
     }
-    if(HEX_OFFSET.size() * 1.2 > columns) {
+    if(PRE_NOTEND.size() > columns) {
         return;
     }
 
     cgh->utils.clearScreen();
-    cgh->utils.outputLine(HEX_OFFSET, cgh->saved_attributes);
-    cgh->utils.outputChar('\n', cgh->saved_attributes);
+    std::string output;
+    std::string ascii_line = " ";
+    output.append(HEX_OFFSET);
+    output.append("\n\n");
 
     int c;
     int off = 0;
     char off_str[9] = {0};
     char curr_byte[4] = {0};
-    COORD saved_pos = {8, 2};
     for(int i = 0; i < rows - 4; i++) {
         for(int j = 0; j < 16; j++) {
-            if(j == 0) {
-                sprintf(off_str, "  %04X: ", off);
-                off += 16;
-                cgh->utils.outputLineNoNew(off_str, cgh->saved_attributes);
-            }
             c = fgetc(f);
             if(c == EOF) {
                 goto end;
             } else {
+                if(j == 0) {
+                    sprintf(off_str, "  %04X: ", off);
+                    off += 16;
+                    output.append(off_str);
+                }
                 sprintf(curr_byte, " %02X", c);
-                cgh->utils.outputLineNoNew(curr_byte, cgh->saved_attributes);
-                saved_pos.X += 3;
-                int value_offset =(15 - j) * 3 + 1 + j;
-                COORD value_pos = {(short)(saved_pos.X + value_offset), saved_pos.Y};
-                SetConsoleCursorPosition(cgh->h_console, value_pos);
-                if(c > 32 && c < 127)
-                    cgh->utils.outputChar(c, cgh->saved_attributes);
-                else
-                    cgh->utils.outputChar('.', cgh->saved_attributes);
-                SetConsoleCursorPosition(cgh->h_console, saved_pos);
+                output.append(curr_byte);
             }
+            if(c > 32 && c < 127)
+                ascii_line.push_back(c);
+            else
+                ascii_line.push_back('.');
         }
-        saved_pos.Y += 1;
-        saved_pos.X = 8;
-        cgh->utils.outputChar('\n', cgh->saved_attributes);
+        output.append(ascii_line);
+        ascii_line = " ";
+        output.push_back('\n');
     }
     if(c != EOF)
-        cgh->utils.outputLine(PRE_NOTEND, cgh->saved_attributes);
+        output.append(PRE_NOTEND);
 end:
+    cgh->utils.outputLineNoNew(output, cgh->saved_attributes);
     waitForClose();
     fclose(f);
     cgh->cleanRedrawConsoleGui();
